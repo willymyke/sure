10fb97cf4a9e3dee306c51fc67a1c8f9a2f72543
MJB <manishimwejeanbatispte3205@gmail.com>
2025-05-20 13:55:30 +0200

    ibra reply app.js

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,25 @@
-ok import React from 'react'
+ import React from 'react'
  
  function App() {
    return (
-umvax mbwir pin nayibagiwe
+     <div>
+    app.get('/select', (req, res) => {
+  const sql = 'SELECT car.plateNumber,car.DriverPhone,payment.AmountPaid FROM payment Join Car on payment.carId = car.carId';
+  db.query(sql, (err, result) => {
+    if (err) {
+      console.error('❌ Select error:', err);
+     res.json({message:'failled to Select'})
+    } else {
+    res.json(result)
+    }
+  });
+});
+
+  ikindi icyo ni question 12 kandi urakor iy field y carId mur table ya car ari primary key ubundi mur payment carId 
+   araba ar foreigin key kand ndashak pin yae nayibagiwe
     </div>
    )
  }
  
  export default App
+
